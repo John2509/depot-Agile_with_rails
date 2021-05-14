a221eea04cdebe7c55965dd7f3085159cf59bd41
Joao Frezzato <jmarcelofo@gmail.com>
2021-05-14 22:03:06 +0000

    Mailing

---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+
+class OrderMailerTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@test.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Prog Ruby/, mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@test.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Prog Ruby<\/td>/, mail.body.encoded
+  end
+
+end
